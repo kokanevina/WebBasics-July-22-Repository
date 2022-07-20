bcd09ed8fedc2074b434298907140b9a6923a53e
kokanevina <kokane.vina@gmail.com>
2022-07-20 17:10:25 +0530

    forEach and map

--- JAVASCRIPT/MyScripts/Methods.js
+++ JAVASCRIPT/MyScripts/Methods.js
@@ -35,3 +35,48 @@ let cubeArrow1=num=>num*num*num;
 let cr=cubeArrow1(5);
 console.log(cr);
 
+// forEach: internal loop
+
+let array=[67000,78000,45000,67000,98000,89000,78000];
+console.log(array);
+
+function callBack(ele,index,ar){
+    console.log("callback called");
+    console.log(ele+" "+index+" "+ar);
+}
+
+array.forEach(callBack);
+array.forEach(function(ele,index,ar){
+    console.log("callback called");
+    console.log(ele+" "+index+" "+ar);
+});
+
+array.forEach(function(ele){
+    console.log(ele);
+});
+array.forEach((ele,index,ar)=>{
+    console.log("callback called");
+    console.log(ele+" "+index+" "+ar);
+});
+
+array.forEach(e=>console.log(e));
+
+// map : visit e->new e -> store in array
+//in the end it will return u that new array
+array=[67000,78000,45000,67000,98000,89000,78000];
+//[69000,80000,47000,69000,100000,91000,80000]
+
+function myLogic(array){
+    let temp=[];
+   for(let ele of array){
+        ele=ele+2000;
+        temp.push(ele);
+   }
+    return temp;
+}
+let resultarray=myLogic(array);
+console.log(array);
+console.log(resultarray);
+
+let resar=array.map(e=>e+2000);
+console.log(resar);
