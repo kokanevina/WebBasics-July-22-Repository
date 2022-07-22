5fe990165d48d4fa54ac4540fa06ff375f16d63c
kokanevina <kokane.vina@gmail.com>
2022-07-22 14:41:53 +0530

    Validation

---
+++ JAVASCRIPT/MyScripts/Validation.js
@@ -0,0 +1,37 @@
+let fnameNode=document.getElementById("fname");
+let ageNode=document.getElementById("age");
+let mobileNode=document.getElementById("contact");
+let emailNode=document.getElementById("mail");
+let passNode=document.getElementById("pass");
+let c_passNode=document.getElementById("confirmpass");
+let unameNode=document.getElementById("uname");
+let errorNode1=document.getElementById("error1");
+let errorNode2=document.getElementById("error2");
+let errorNode3=document.getElementById("error3");
+let errorNode4=document.getElementById("error4");
+let errorNode5=document.getElementById("error5");
+let errorNode6=document.getElementById("error6");
+let errorNode7=document.getElementById("error7");
+
+
+function validate1(){
+
+}
+function validate2(){
+    
+}
+function validate3(){
+    
+}
+function validate4(){
+    
+}
+function validate5(){
+    
+}
+function validate6(){
+    
+}
+function validate7(){
+    
+}
